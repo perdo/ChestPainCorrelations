b3f84bb232b59df0f38506a907ca54c676fb4b66
Shawn Biesan <2perdo@gmail.com>
2013-08-20 12:53:58 -0400

    Removes extra edges

--- generate_date_deltas.py
+++ generate_date_deltas.py
@@ -10,7 +10,8 @@ def generate_delta_plot(data_file, day_length=1, start=0):
     deltas = []
     for line in input_file:
         counts.append(int(line['Count']))
-    deltas = [counts[pos] - counts[pos-day_length] for pos in range(day_length+start, len(counts))]
+    left_over_values = (len(counts)-start) % day_length
+    deltas = [counts[pos] - counts[pos-day_length] for pos in range(day_length+start, len(counts) - left_over_values)]
 
     pylab.hist(deltas, normed=True)
     pylab.title("Spaced by %s days" % (day_length,))
@@ -27,11 +28,12 @@ def choose_best_cycle(data_file):
     #print counts
     for k in range(1, 31):
     #for k in range(1, 3):
-        w = []
         start_error = {}
         error = 0
         for start in range(0, k): #issue, strips off anything that doesn't fit on left side but not right side maybe I made it other side issue? 
-            error_terms = [1.0 * abs(counts[pos] - counts[pos-k]) for pos in range(k+start, len(counts))]
+            left_over_values = (len(counts)-start) % k
+            error_terms = [1.0 * abs(counts[pos] - counts[pos-k]) for pos in range(k+start, len(counts)-left_over_values)]
+            #error_terms = [1.0 * abs(counts[pos] - counts[pos-k]) for pos in range(k+start, len(counts))]
             error = 1.0 * sum(error_terms) / len(error_terms)
             start_error[start] = error
         best_start_key = min(start_error.iteritems(), key=operator.itemgetter(1))[0]
