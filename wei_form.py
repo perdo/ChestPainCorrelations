f4da6fcb4122d21d95a82c994844a4edf8a697b3
Shawn Biesan <2perdo@gmail.com>
2013-08-30 17:20:49 -0400

    General cleanup and refactoring

--- wei_form.py
+++ wei_form.py
@@ -6,25 +6,6 @@ import pylab
 import operator
 import math
 
-#def output_form_single(data_file):
-#    counts = []
-#    counts_fem = []
-#    counts_male = []
-#    input_file = csv.DictReader(open(data_file))
-#
-#    for line in input_file:
-#        counts_male.append(int(line['Patient Count Male']))
-#        counts_fem.append(int(line['Patient Count Female']))
-#    
-#    name = 'output_test_male.csv'
-#    writer = csv.writer(open(name, 'wt'))
-#    for i in range(0, int(math.ceil(len(counts_male))/2.0) + 1):
-#        writer.writerow([counts_male[j] for j in range(i, len(counts_male) - i)])
-#    name = 'output_test_female.csv'
-#    writer = csv.writer(open(name, 'wt'))
-#    for i in range(0, int(math.ceil(len(counts_male))/2.0) + 1):
-#        writer.writerow([counts_fem[j] for j in range(i, len(counts_fem) - i)])
-
 def output_form_regions(regions):
     for region in regions:
         counts = []
@@ -39,31 +20,26 @@ def output_form_regions(regions):
                     counts_ages[entry].append(int(line[entry]))
             counts_male.append(int(line['Patient Count Male']))
             counts_fem.append(int(line['Patient Count Female']))
-        
-        name = "output_%s_male.csv" %(region,)
-        writer = csv.writer(open(name, 'wt'))
-        for i in range(0, int(math.ceil(len(counts_male))/2.0) + 1):
-            writer.writerow([counts_male[j] for j in range(i, len(counts_male) - i)]) # prints correct portion of data for auto regression
-        name = "output_%s_female.csv" %(region,)
-        writer = csv.writer(open(name, 'wt'))
-        for i in range(0, int(math.ceil(len(counts_male))/2.0) + 1):
-            writer.writerow([counts_fem[j] for j in range(i, len(counts_fem) - i)]) 
-        for entry in counts_ages.keys():
-            if len(counts_ages[entry]) == 0: # No need for extra files if the data is empty. Maybe threshold this to like 10?
+        output_form_single(region, counts_fem, 'female')
+        output_form_single(region, counts_male, 'male')
+        for age_key in counts_ages.keys():
+            if len(counts_ages[age_key]) == 0: # No need for extra files if the data is empty. Maybe threshold this to like 10?
                 continue
-            name = "output_%s_%s.csv" %(region, entry)
-            writer = csv.writer(open(name, 'wt'))
-            for i in range(0, int(math.ceil(len(counts_ages[entry]))/2.0) + 1):
-                writer.writerow([counts_ages[entry][j] for j in range(i, len(counts_ages[entry]) - i)])
+            output_form_single(region, counts=counts_ages[age_key], age_key=age_key)
 
+def output_form_single(region, counts, gender=None, age_key=None):
+    """  """
+    if gender and gender in ('female', 'male',):
+        name = "output_%s_%s.csv" %(region, gender,)
+    elif age_key:
+        name = "output_%s_%s.csv" %(region, age_key,)
+    writer = csv.writer(open(name, 'wt'))
+    for i in range(0, int(math.ceil(len(counts))/2.0) + 1):
+        writer.writerow([counts[j] for j in range(i, len(counts) - i)]) # prints correct portion of data for auto regression
 
                     
 
 def main():
-    #if len(sys.argv) == 2:
-    #    data_file = sys.argv[1]
-    #    output_form_single(data_file)
-    #else:
     regions = ('OregonEugeneSpringfield', 'WashingtonLongview', 'WashingtonWhatcom', )
     output_form_regions(regions)
 
